253807c2b60711f9ed3e96e9654048863b55fb34
Mateen Mirani <mateenmirani125@gmail.com>
2023-07-26 16:28:55 +0500

    add GetStarted, Hero, & Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={`flex flex-col md:flex-row ${styles.paddingY}`}>
+      <div
+        className={` flex-1 flex-col xl:px-0 sm:px-16 px-6 ${styles.flexStart}`}
+      >
+        <div className="flex flex-row items-center px-4 py-[6px] bg-discount-gradient rounded-[10px] mb-2">
+          <img
+            src={discount}
+            alt="discount icon"
+            className="w-[32px] h-[32px]"
+          />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For{" "}
+            <span className="text-white">1 Month </span> Account
+          </p>
+        </div>
+
+        {/* div for heading */}
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold text-[52px] ss:text-[72px] text-white leading-[75px] ss:leading-[100px]">
+            The Next <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">Generation</span>{" "}
+          </h1>
+
+          {/* div for the button */}
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        {/* Heading below the button */}
+        <h1 className="font-poppins font-semibold text-[52px] ss:text-[68px] text-white leading-[75px] ss:leading-[100px]">
+          Payment Method.
+        </h1>
+
+        {/* Paragraph below the heading */}
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae
+          veniam rerum sint debitis delectus molestiae? Amet quidem
+          necessitatibus et quo?
+        </p>
+      </div>
+
+      {/* div for the image side */}
+      <div>
+        <img
+          src={robot}
+          alt="Billing"
+          className="relative w-[100%] h-[100%] z-[5]"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,61 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const showMenuHandler = () => setShowMenu((prev) => !prev);
   return (
-    <nav className="w-full flex justify-between items-center py-6 navbar"></nav>
+    <nav className="w-full flex justify-between items-center py-6 navbar">
+      <img
+        src={logo}
+        alt="hoobank"
+        className="w-[124px] h-[32px] cursor-pointer"
+      />
+
+      {/* Nav links for large screens */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal text-[16px] cursor-pointer text-white ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* Navbar for Mobile screen */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={showMenu ? close : menu}
+          alt="humburger button"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={showMenuHandler}
+        />
+
+        {/* div for showing the links */}
+        <div
+          className={`${
+            showMenu ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal text-[16px] cursor-pointer text-white hover:text-blue-400 ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
